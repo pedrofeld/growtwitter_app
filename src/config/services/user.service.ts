e74bd3e0d194f5a2d92ca2badcb8a8b6810e1e45
Pedro Feld <pedro.gael.feld@gmail.com>
2026-02-08 17:50:13 -0300

    feat: add register, update and delete users methods

--- src/config/services/user.service.ts
+++ src/config/services/user.service.ts
@@ -3,6 +3,55 @@ import type { ResponseDto } from "../dtos/response.dto";
 import apiService, { api } from "./api.service";
 
 class UserService {
+    public async register(
+        name: string,
+        username: string,
+        email: string,
+        password: string,
+        profileImage?: string,
+    ): Promise<ResponseDto> {
+        try {
+            const result = await api.post(`/user`, {
+                name,
+                username,
+                email,
+                password,
+                profileImage,
+            });
+            return {
+                ok: true,
+                ...result.data,
+            }
+        } catch (error: any) {
+            return apiService.handleError(error);
+        }
+    }
+
+    public async updateProfile(
+        id: string,
+        name?: string,
+        username?: string,
+        email?: string,
+        password?: string,
+        profileImage?: string,
+    ): Promise<ResponseDto> {
+        try {
+            const result = await api.put(`/user/${id}`, {
+                name,
+                username,
+                email,
+                password,
+                profileImage,
+            });
+            return {
+                ok: true,
+                ...result.data,
+            }
+        } catch (error: any) {
+            return apiService.handleError(error);
+        }
+    }
+
     public async login (
         username: string,
         password: string
@@ -13,6 +62,20 @@ class UserService {
                 password,
             });
 
+            return {
+                ok: true,
+                ...result.data,
+            }
+        } catch (error: any) {
+            return apiService.handleError(error);
+        }
+    }
+
+    public async deleteUser(
+        id: string,
+    ) : Promise<ResponseDto> {
+        try {
+            const result = await api.delete(`/user/${id}`);
             return {
                 ok: true,
                 ...result.data,
