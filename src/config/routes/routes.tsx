45b185b5f8436f9800a0cb5241ab358ae29c7422
Pedro Feld <pedro.gael.feld@gmail.com>
2026-02-10 09:16:40 -0300

    security: add ProtectedRoute

--- src/config/routes/routes.tsx
+++ src/config/routes/routes.tsx
@@ -4,28 +4,33 @@ import { DefaultLayout } from "../layout/DefaultLayout";
 import { FeedPage } from "../../pages/Feed";
 import { ExplorePage } from "../../pages/Explore";
 import { ProfilePage } from "../../pages/Profile";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 export const routes = createBrowserRouter([
-    {
-        path: "/login",
-        element: <LoginPage />,
-    },
-    {
-        path: "/",
-        element: <DefaultLayout />,
-        children: [
-            {
-                path: "",
-                element: <FeedPage />,
-            },
-            {
-                path: "explore",
-                element: <ExplorePage />,
-            },
-            {
-                path: "profile",
-                element: <ProfilePage />,
-            }
-        ]
-    }
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <DefaultLayout />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: "",
+        element: <FeedPage />,
+      },
+      {
+        path: "explore",
+        element: <ExplorePage />,
+      },
+      {
+        path: "profile",
+        element: <ProfilePage />,
+      },
+    ],
+  },
 ]);
