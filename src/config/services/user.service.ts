65bfa0e7d2f155cb15d6b560d0a174ace7d76c39
Pedro Feld <pedro.gael.feld@gmail.com>
2026-02-11 12:38:43 -0300

    fix: login route

--- src/config/services/user.service.ts
+++ src/config/services/user.service.ts
@@ -3,87 +3,82 @@ import type { ResponseDto } from "../dtos/response.dto";
 import apiService, { api } from "./api.service";
 
 class UserService {
-    public async register(
-        name: string,
-        username: string,
-        email: string,
-        password: string,
-        profileImage?: string,
-    ): Promise<ResponseDto> {
-        try {
-            const result = await api.post(`/user`, {
-                name,
-                username,
-                email,
-                password,
-                profileImage,
-            });
-            return {
-                ok: true,
-                ...result.data,
-            }
-        } catch (error: any) {
-            return apiService.handleError(error);
-        }
+  public async register(
+    name: string,
+    username: string,
+    email: string,
+    password: string,
+    profileImage?: string,
+  ): Promise<ResponseDto> {
+    try {
+      const result = await api.post(`/user`, {
+        name,
+        username,
+        email,
+        password,
+        profileImage,
+      });
+      return {
+        ok: true,
+        ...result.data,
+      };
+    } catch (error: any) {
+      return apiService.handleError(error);
     }
+  }
 
-    public async updateProfile(
-        id: string,
-        name?: string,
-        username?: string,
-        email?: string,
-        password?: string,
-        profileImage?: string,
-    ): Promise<ResponseDto> {
-        try {
-            const result = await api.put(`/user/${id}`, {
-                name,
-                username,
-                email,
-                password,
-                profileImage,
-            });
-            return {
-                ok: true,
-                ...result.data,
-            }
-        } catch (error: any) {
-            return apiService.handleError(error);
-        }
+  public async updateProfile(
+    id: string,
+    name?: string,
+    username?: string,
+    email?: string,
+    password?: string,
+    profileImage?: string,
+  ): Promise<ResponseDto> {
+    try {
+      const result = await api.put(`/user/${id}`, {
+        name,
+        username,
+        email,
+        password,
+        profileImage,
+      });
+      return {
+        ok: true,
+        ...result.data,
+      };
+    } catch (error: any) {
+      return apiService.handleError(error);
     }
+  }
 
-    public async login (
-        username: string,
-        password: string
-    ): Promise<ResponseDto> {
-        try {
-            const result = await api.post('/auth/login', {
-                username,
-                password,
-            });
+  public async login(username: string, password: string): Promise<ResponseDto> {
+    try {
+      const result = await api.post("/login", {
+        username,
+        password,
+      });
 
-            return {
-                ok: true,
-                ...result.data,
-            }
-        } catch (error: any) {
-            return apiService.handleError(error);
-        }
+      return {
+        ok: true,
+        ...result.data,
+      };
+    } catch (error: any) {
+      return apiService.handleError(error);
     }
+  }
 
-    public async deleteUser(
-        id: string,
-    ) : Promise<ResponseDto> {
-        try {
-            const result = await api.delete(`/user/${id}`);
-            return {
-                ok: true,
-                ...result.data,
-            }
-        } catch (error: any) {
-            return apiService.handleError(error);
-        }
+  public async deleteUser(id: string): Promise<ResponseDto> {
+    try {
+      const result = await api.delete(`/user/${id}`);
+      return {
+        ok: true,
+        ...result.data,
+      };
+    } catch (error: any) {
+      return apiService.handleError(error);
     }
+  }
 }
 
 export default new UserService();
