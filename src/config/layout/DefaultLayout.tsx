9e272fd3cb8c069792e21ae736e30dd02d844f41
Pedro Feld <pedro.gael.feld@gmail.com>
2026-02-17 11:10:27 -0300

    fix: removing content of DefaultLayout (displaying only as a visual caste and rendering the child pages)

--- src/config/layout/DefaultLayout.tsx
+++ src/config/layout/DefaultLayout.tsx
@@ -1,7 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 export const DefaultLayout = () => {
-    return (
-        <div>
-            <h1>Default Layout</h1>
-        </div>
-    );
-}
+  return <Outlet />;
+};
